f06743320b2d3dbed6e08bd1b588be8689dc7e27
John David Back <johndavidback@gmail.com>
2010-09-26 21:21:15 +0000

    added cart class and imported in views.py

---
+++ cart.py
@@ -0,0 +1,5 @@
+class Cart:
+	""" This is the cart class, to be used for the cart.  Make sense? """
+	def __init__(self):
+		self.total = 0
+		self.items = {}
--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,8 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 
+import SPyCart.cart
+
 def index(request):
 
 	return render_to_response('cart/index.html')
